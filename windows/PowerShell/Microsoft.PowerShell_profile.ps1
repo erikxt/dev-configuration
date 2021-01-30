1de64348b3b466f356894fac7c29c3f8686a001e
erik <iqboy118@gmail.com>
2021-01-30 14:49:24 +0800

    modify powershell configuration

--- windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,44 @@
-Import-Module posh-git # 引入 posh-git
-Import-Module oh-my-posh # 引入 oh-my-posh
+Import-Module Get-ChildItemColor
 
-Set-Theme Sorin # 设置主题为 Sorin
+$env:PYTHONIOENCODING="utf-8"
+# Remove curl alias
+If (Test-Path Alias:curl) {Remove-Item Alias:curl}
+If (Test-Path Alias:curl) {Remove-Item Alias:curl}
+# Remove-Item alias:ls -force
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+function GitLogPretty {
+  git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --all
+}
+
+function PrettyLS {
+  colorls --light -A
+}
+
+function GitStat {git status}
+
+function GoBack {Set-Location ..}
+
+function GetMyIp {curl -L tool.lu/ip}
+function UpdateScoop {scoop update; scoop update *}
+
+function UpdateChoco {choco upgrade chocolatey}
+
+Import-Module posh-git
+Import-Module oh-my-posh
+# $DefaultUser = 'spenc'
+
+# Setup other alias
+Set-Alias open Invoke-Item
+Set-Alias .. GoBack
+Set-Alias glola GitLogPretty
+Set-Alias gst GitStat
+Set-Alias myip GetMyIp
+Set-Alias pls PrettyLS
+
+# Set theme
+Set-Theme Sorin
 
 Set-PSReadLineOption -PredictionSource History # 设置预测文本来源为历史记录
  
